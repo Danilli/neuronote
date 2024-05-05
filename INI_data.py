ce0be1c6f30804760c824df9310794ce0da6d169
Danya <jjeymaytvey@mail.ru>
2024-05-05 15:18:48 +0300

    Заполнение БД данными из Фикбука

--- INI_data.py
+++ INI_data.py
@@ -1,10 +1,32 @@
 from pyArango.connection import *
+import requests
+from bs4 import BeautifulSoup
+
 
 '''
 https://pyarango.readthedocs.io/en/latest/
 https://arangodb.com/tutorials/tutorial-python/
 '''
-def add_data_to_db(collection):
+
+def take_fic_janr():
+    genre_titles = []
+    for i in [1, 2]:
+        url = f"https://ficbook.net/tag-categories-8533967/25?p={i}"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        }
+
+        response = requests.get(url, headers=headers)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        genre_elements = soup.find_all('a', class_='font-bold')
+
+        genre_titles += [genre_element.get('title') for genre_element in genre_elements]
+
+    return genre_titles
+
+def add_test_data_to_db(collection):
     # Добавление данных в коллекцию
     data1 = {
         "Tags": ["Юридический", "Научный"],
@@ -50,26 +72,100 @@ def add_data_to_db(collection):
     new_doc4.save()
     print("Данные успешно добавлены в коллекцию 'Articles'.")
 
+
 def read_all_docs(documents):
     for doc in documents.fetchAll():
         print(doc)
+    return documents.fetchAll()
 
-conn = Connection(username="root", password="123")
 
-#db = conn.createDatabase(name="ProjectDB")
+def create_genre_coll(db):
+    Collection = db.createCollection(name="Genres")
+    return Collection
 
-db = conn["ProjectDB"]
 
-print(db)
+def create_fanf_coll(db):
+    Collection = db.createCollection(name="Fanfics")
+    return Collection
 
-#ArticlesCollection = db.createCollection(name="Articles")
-#Debug
-Articles = db["Articles"]
 
-print(Articles)
+def fill_genre_collection(db):
+    Genres = db["Genres"]
 
-print(type(Articles))
+    data = {
+        "Title": "Genres",
+        "list_of_genres": str(take_fic_janr())
+    }
 
+    new_doc = Genres.createDocument(data)
+    new_doc.save()
+    print(data)
+    return data
+
+
+def make_conn_to_db(username="root", password="123", db_name = "ProjectDB"):
+    conn = Connection(username=username, password=password)
+    db = conn[db_name]
+    return db
+
+
+def take_fic_info(pg_num=1, genre_titles = []):
+
+    if not genre_titles: return {}
+
+    url = f"https://ficbook.net/fanfiction?p={pg_num}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    fanfics = soup.find_all('div', class_='js-toggle-description')
+    genre_titles = set(genre_titles)
+
+    data = []
+
+    for fic in fanfics:
+        try:
+            title = fic.find('a', class_='visit-link').text.strip()
+
+            # Находим метки
+            tags = fic.find('div', class_='tags').text.strip().split('\n\n', 2)[0].split(':\n', 2)[1]
+            tags = set(tags.split('\n'))
+            tags = list(tags.intersection(genre_titles))
+
+            if not tags:
+                continue
+
+            # Находим аннотацию книги
+            annotation = fic.find('div', class_='fanfic-description').text.strip()
+            data += [{
+                'Название': title,
+                'Метки': tags,
+                'Аннотация': annotation
+            }]
+        except: continue
+    print(data)
+    return data
+
+
+def fill_ficInfo_collection(db):
+    docs = read_all_docs(db["Genres"])
+    genres = docs[0]["list_of_genres"].replace("[", "").replace("]", "").replace("'", "").split(", ")
+    print(genres)
+    fanf = create_fanf_coll(db)
+    fanf = db["Fanfics"]
+
+    for p in range(1, 50):
+        page_data = take_fic_info(p, genres)
+        for data in page_data:
+            new_doc = fanf.createDocument(data)
+            new_doc.save()
+
+
+db = make_conn_to_db()
+create_genre_coll(db)
+fill_genre_collection(db)
+fill_ficInfo_collection(db)
 
 
 
